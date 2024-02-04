8567b6026ea4dd81ad3d2b1a21b0afea790a10d1
Ezinw <112014367+Ezinw@users.noreply.github.com>
2024-02-04 10:09:31 -0400

    Add files via upload

---
+++ src/Settings.cs
@@ -0,0 +1,28 @@
+﻿using ModSettings;
+
+namespace SleepWithoutABed
+{
+
+    internal class Settings : JsonModSettings
+    {
+        public enum Choice
+        {
+            Default, ThreeQuarters, Half, Quarter, Eighth
+        }
+
+        [Name("Sleeping penalty when not using a bed")]
+        [Description("Set the penalty to the amount of fatigue restored while sleeping without a bed - (Mod default = 1/2 of TLD default value)")]
+        [Choice("TLD Default", "3/4 of default", "1/2 of default", "1/4 of default", "1/8 of default")]
+        public Choice sleepPenalty = Choice.Half;
+
+
+        internal static Settings settings;
+        internal static void OnLoad()
+        {
+            settings = new Settings();
+            settings.AddToModSettings("SleepWithoutABed");
+        }
+    
+    }
+
+}
