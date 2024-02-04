45d4d74e1c98ebc02b077614ce7ee0138ddadea8
Ezinw <112014367+Ezinw@users.noreply.github.com>
2024-02-04 10:09:40 -0400

    Update Implementation.cs

--- src/Implementation.cs
+++ src/Implementation.cs
@@ -5,7 +5,9 @@ namespace SleepWithoutABed
     internal sealed class Implementation : MelonMod
     {
         public override void OnInitializeMelon()
-        { }
+        { 
+            Settings.OnLoad();
+        }
 
     }
 
